c43f1a5ae2e8be36f74c79f70f4580a8993262d2
DallinVetter <dalljvetter@gmail.com>
2022-03-27 19:28:02 -0600

    Admin layout

--- front-end/src/views/AdminView.vue
+++ front-end/src/views/AdminView.vue
@@ -1,61 +1,74 @@
 <template>
   <div class="admin">
     <h1>The Admin Page!</h1>
-    <div class="heading">
-      <div class="circle">1</div>
-      <h2>Add an Item</h2>
-    </div>
-    <div class="add">
-      <div class="form">
-        <div>
-          <input v-model="title" placeholder="Title" />
-        </div>
-        <div>
-          <input v-model="description" placeholder="Description" />
-        </div>
-        <p></p>
-        <input type="file" name="photo" @change="fileChanged" />
-        <button @click="upload">Upload</button>
+    <button @click="managerSelection(true)">Product Management</button>
+    <button @click="managerSelection(false)">Service Management</button>
+    <div v-if="manageOption">
+      <div class="heading">
+        <div class="circle">1</div>
+        <h2>Add Landscaping Product</h2>
       </div>
-      <div class="upload" v-if="addItem">
-        <h2>{{ addItem.title }}</h2>
-        <h2>{{ addItem.description }}</h2>
-        <img :src="addItem.path" />
+      <div class="add">
+        <div class="form">
+          <div>
+            <input v-model="title" placeholder="Product Title" />
+          </div>
+          <div>
+            <input v-model="description" placeholder="Product Description" />
+          </div>
+          <div>
+            <input v-model="price" placeholder="Product Price" />
+          </div>
+          <p></p>
+          <input type="file" name="photo" @change="fileChanged" />
+          <button @click="upload">Upload</button>
+        </div>
+        <div class="upload" v-if="addItem">
+          <h2>{{ addItem.title }}</h2>
+          <h2>{{ addItem.description }}</h2>
+          <img :src="addItem.path" />
+        </div>
       </div>
-    </div>
 
-    <div class="heading">
-      <div class="circle">2</div>
-      <h2>Edit/Delete an Item</h2>
-    </div>
-    <div class="edit">
-      <div class="form">
-        <input v-model="findTitle" placeholder="Search" />
-        <div class="suggestions" v-if="suggestions.length > 0">
-          <div
-            class="suggestion"
-            v-for="s in suggestions"
-            :key="s.id"
-            @click="selectItem(s)"
-          >
-            {{ s.title }}
+      <div class="heading">
+        <div class="circle">2</div>
+        <h2>Edit/Delete Product</h2>
+      </div>
+      <div class="edit">
+        <div class="form">
+          <input v-model="findTitle" placeholder="Search" />
+          <div class="suggestions" v-if="suggestions.length > 0">
+            <div
+              class="suggestion"
+              v-for="s in suggestions"
+              :key="s.id"
+              @click="selectItem(s)"
+            >
+              {{ s.title }}
+            </div>
           </div>
         </div>
-      </div>
-      <div class="upload" v-if="findItem">
-        <div>
-          <input v-model="findItem.title" />
+        <div class="upload" v-if="findItem">
+          <div>
+            <input v-model="findItem.title" />
+          </div>
+          <div>
+            <textarea v-model="findItem.description" />
+          </div>
+          <div>
+            <textarea v-model="findItem.price" />
+          </div>
+          <p></p>
+          <img :src="findItem.path" />
         </div>
-        <div>
-          <textarea v-model="findItem.description" />
+        <div class="actions" v-if="findItem">
+          <button @click="deleteItem(findItem)">Delete</button>
+          <button @click="editItem(findItem)">Edit</button>
         </div>
-        <p></p>
-        <img :src="findItem.path" />
       </div>
-      <div class="actions" v-if="findItem">
-        <button @click="deleteItem(findItem)">Delete</button>
-        <button @click="editItem(findItem)">Edit</button>
-      </div>
+    </div>
+    <div v-else>
+      <p>Else works</p>
     </div>
   </div>
 </template>
@@ -69,11 +82,13 @@ export default {
     return {
       title: "",
       description: "",
+      price: "",
       file: null,
       addItem: null,
       items: [],
       findTitle: "",
       findItem: null,
+      manageOption: false,
     };
   },
   computed: {
@@ -102,6 +117,9 @@ export default {
         console.log(error);
       }
     },
+    managerSelection(status) {
+      this.manageOption = status;
+    },
     async upload() {
       try {
         const formData = new FormData();
@@ -123,6 +141,7 @@ export default {
         await axios.put("/api/items/" + item._id, {
           title: this.findItem.title,
           description: this.findItem.description,
+          price: this.findItem.price,
         });
         this.findItem = null;
         this.getItems();
